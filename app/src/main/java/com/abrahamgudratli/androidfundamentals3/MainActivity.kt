b1ef1cc525b0a1c108a7a045a8a4e7c6328ea613
Ibrahim <igudratli4680@ada.edu.az>
2022-11-06 20:30:44 +0400

    third commit

--- app/src/main/java/com/abrahamgudratli/androidfundamentals3/MainActivity.kt
+++ app/src/main/java/com/abrahamgudratli/androidfundamentals3/MainActivity.kt
@@ -3,10 +3,7 @@ package com.abrahamgudratli.androidfundamentals3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.RadioButton
-import android.widget.RadioGroup
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,7 +24,7 @@ class MainActivity : AppCompatActivity() {
         val garlic = checkGarlic.isChecked
 
         val orderString = "Your order is ${meat.text} with ${if (onion) "\nonion" else ""} ${if (tomato) "\ntomato" else ""} ${if (garlic) "\ngarlic" else ""}"
-        textView2.text = orderString
+        tvShowOrder.text = orderString
 
     }
 }
